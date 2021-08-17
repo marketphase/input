af36a5a361218452751c6844073a400053eb7819
pieterDekker <pieter.dekker20@gmail.com>
2021-08-17 16:01:33 +0200

    Add Forgiving/ObjectNode.php

---
+++ src/Node/Forgiving/ObjectNode.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Linio\Component\Input\Node\Forgiving;
+
+use Linio\Component\Input\InputError;
+
+class ObjectNode extends BaseNode
+{
+    protected function containsError(array $values): bool
+    {
+        foreach ($values as $value) {
+            if ($value instanceof InputError) {
+                return true;
+            }
+            if (is_array($value)) {
+                return $this->containsError($value);
+            }
+        }
+        return false;
+    }
+
+    public function getValue(string $field, $value)
+    {
+        $error = $this->checkConstraintsWithError($field, $value);
+        if ($error instanceof InputError) {
+            return $error;
+        }
+        if (is_array($value) && $this->containsError($value)) {
+            return $value;
+        }
+
+        return $this->instantiator->instantiate($this->type, $value);
+    }
+}
