01e1179c5a3dd60a6b8793633caddfed859431f3
pieterDekker <pieter.dekker20@gmail.com>
2021-08-30 15:09:47 +0200

    - Rolled test for exact array into ForgivingInputHandlerTest::testNestedErrorIsFound - Rolled test for exact message into ForgivingInputHandlerTest::testReturnsInvalid - Added messages to all assertions

--- tests/ForgivingInputHandler/ForgivingInputHandlerTest.php
+++ tests/ForgivingInputHandler/ForgivingInputHandlerTest.php
@@ -25,9 +25,9 @@ class ForgivingInputHandlerTest extends TestCase
                 'branch' => 'IT',
             ],
         ]);
-        self::assertFalse($inputHandler->hasErrorFor('key1'));
-        self::assertFalse($inputHandler->hasErrorFor('key2'));
-        self::assertTrue($inputHandler->hasErrorFor('company'));
+        $this->assertTrue($inputHandler->hasErrorFor('company'));
+        $reasonArray = $inputHandler->getErrorFor('company')->getReason();
+        $this->assertEquals(['ceo' => ['lastName' => 'We need input for the last name of the CEO']], $reasonArray);
     }
 
     public function testAllowsIncompleteInput()
@@ -40,9 +40,10 @@ class ForgivingInputHandlerTest extends TestCase
             }
         };
         $inputHandler->bind(['key1' => 'value1']);
-        $this->assertTrue($inputHandler->hasData('key1'));
-        $this->assertFalse($inputHandler->hasData('key2'));
-        $this->assertTrue($inputHandler->hasErrorFor('key2'));
+        $this->assertTrue($inputHandler->hasData('key1'), "Expected to have actual data for the first key");
+        $this->assertFalse($inputHandler->hasData('key2'), "Unexpected data for the second key");
+        $this->assertTrue($inputHandler->hasErrorFor('key2'), "Expected to have an error for the second key");
+        $this->assertInstanceOf(Missing::class, $inputHandler->getErrorFor('key2'), "Expected the error for the second key to be Missing");
     }
 
     public function testAllowsPartiallyInvalidInput()
@@ -55,9 +56,10 @@ class ForgivingInputHandlerTest extends TestCase
             }
         };
         $inputHandler->bind(['key1' => 'value1', 'key2' => 0]);
-        $this->assertTrue($inputHandler->hasData('key1'));
-        $this->assertFalse($inputHandler->hasData('key2'));
-        $this->assertTrue($inputHandler->hasErrorFor('key2'));
+        $this->assertTrue($inputHandler->hasData('key1'), "Expected to have actual data for the first key");
+        $this->assertFalse($inputHandler->hasData('key2'), "Unexpected data for the second key");
+        $this->assertTrue($inputHandler->hasErrorFor('key2'), "Expected to have an error for the second key");
+        $this->assertInstanceOf(Invalid::class, $inputHandler->getErrorFor('key2'), "Expected the error for the second key to be Invalid");
     }
 
     public function testReturnsMissing()
@@ -71,9 +73,9 @@ class ForgivingInputHandlerTest extends TestCase
         };
         $inputHandler->bind(['key1' => 'value1']);
         $error = $inputHandler->getErrorFor('key2');
-        $this->assertNotNull($error);
-        $this->assertInstanceOf(Missing::class, $error);
-        $this->assertEquals('key2', $error->field);
+        $this->assertNotNull($error, "Expected to have an error for the second key");
+        $this->assertInstanceOf(Missing::class, $error, "Expected the error for the second key to be Missing");
+        $this->assertEquals('key2', $error->field, "Expected the error for the second key to mention the field name 'key2'");
     }
 
     public function testReturnsInvalid()
@@ -87,42 +89,14 @@ class ForgivingInputHandlerTest extends TestCase
         };
         $inputHandler->bind(['key1' => 'value1', 'key2' => 0]);
         $error = $inputHandler->getErrorFor('key2');
-        $this->assertNotNull($error);
-        $this->assertInstanceOf(Invalid::class, $error);
-        $this->assertEquals('key2', $error->field);
-    }
-
-    public function testReasonString()
-    {
-        $inputHandler = new class () extends ForgivingInputHandler {
-            public function define()
-            {
-                $this->add('key1', 'string');
-                $this->add('key2', 'string');
-            }
-        };
-        $inputHandler->bind(['key1' => 'value1', 'key2' => 0]);
+        $this->assertNotNull($error, "Expected to have an error for the second key");
+        $this->assertInstanceOf(Invalid::class, $error, "Expected the error for the second key to be Invalid");
+        $this->assertEquals('key2', $error->field, "Expected the error for the second key to mention the field name 'key2'");
         $reasonString = $inputHandler->getErrorFor('key2')->getReason();
-        self::assertEquals('[key2] Value does not match type: string', $reasonString);
-    }
-
-    public function testReasonArray()
-    {
-        $inputHandler = new CompanyInputHandler();
-        $inputHandler->bind([
-            'action' => 'found',
-            'ceo' => [
-                'firstName' => 'John',
-                'lastName' => 'Doe',
-            ],
-            'company' => [
-                'ceo' => [
-                    'firstName' => 'John',
-                ],
-                'branch' => 'IT',
-            ],
-        ]);
-        $reasonArray = $inputHandler->getErrorFor('company')->getReason();
-        self::assertEquals(['child3' => ['child2' => 'We need input for a second child']], $reasonArray);
+        $this->assertEquals(
+            '[key2] Value does not match type: string',
+            $reasonString,
+            "Expected the reason string to be '[key2] Value does not match type: string', got $reasonString"
+        );
     }
 }
