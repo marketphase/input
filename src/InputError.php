f7f56532dc20e67ed5dd1de988192eb857172709
pieterDekker <pieter.dekker20@gmail.com>
2021-09-13 14:25:12 +0200

    Update InputError to give proper reason arrays for scalar collections with errors

--- src/InputError.php
+++ src/InputError.php
@@ -16,14 +16,36 @@ abstract class InputError
         if (is_string($this->reason)) {
             return $this->reason;
         }
+        if (is_a($this->reason, ScalarCollectionItemInvalid::class)) {
+            return $this->reason->getReason();
+        }
         if (is_a($this->reason, InputError::class)) {
             return [$this->field => $this->reason->getReason()];
         }
         $reasons = [];
-        /** @var InputError $singleReason */
-        foreach ($this->reason as $singleReason) {
-            $reasons[$singleReason->field] = $singleReason->getReason();
+        if ($this->containsScalarCollectionInputError($this->reason)) {
+            $reasons[$this->field] = [];
+            foreach ($this->reason as $singleReason) {
+                assert($singleReason instanceof ScalarCollectionItemInvalid);
+                $reasons[$this->field][$singleReason->index] = $singleReason->getReason();
+            }
+        } else {
+            /** @var InputError $singleReason */
+            foreach ($this->reason as $singleReason) {
+                $reasons[$singleReason->field] = $singleReason->getReason();
+            }
         }
+
         return $reasons;
+    }
+
+    private function containsScalarCollectionInputError(array $reasons): bool
+    {
+        foreach ($reasons as $reason) {
+            if ($reason instanceof ScalarCollectionItemInvalid) {
+                return true;
+            }
+        }
+        return false;
     }
 }
